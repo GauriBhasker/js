f5b55d5b350f75ede7a37aa83b9baf3894b877c8
Gauri <68182889+GauriBhasker@users.noreply.github.com>
2023-10-12 06:55:13 +0000

    datatypes

---
+++ js_basics/datatypes.js
@@ -0,0 +1,22 @@
+/* primitive data types
+string   "hi"
+number   74325
+boolean  true or false
+bigint  for large numbers more than 2^52
+null    e.g. let name = null   
+undefined     e.g. let name;  now name is declared but isn't assigned anything.. so it is undefined
+symbol   unique
+*/
+
+/* non-primitive data type
+object 
+null also lies under category of object
+*/
+
+//typeof is used to check the data type of var and let
+
+
+console.log(typeof 8);
+console.log(typeof "name");
+console.log(typeof true);
+console.log(typeof "true");
