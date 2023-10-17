7702e94f139dc87ccd7873c0064162bc588f746d
Gauri <68182889+GauriBhasker@users.noreply.github.com>
2023-10-17 14:04:13 +0000

    strings

---
+++ js_basics/string.js
@@ -0,0 +1,23 @@
+console.log("Arun is a 'singer'")
+console.log('Arun is a "singer"');
+
+
+let name = "Arun"
+let age = 22
+
+console.log(`My name is ${name} and my age is ${age}`);
+
+
+const myname = new String("Hello my name is suzy");
+
+console.log(myname)
+
+console.log(myname[0])
+
+let mystring = "      hello     "
+console.log(mystring.trim())
+console.log(mystring.charAt(4))
+console.log(myname.split('m'))
+
+let str = "bam bam"
+console.log(str.replace('b','r'));
