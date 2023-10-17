c57f80361ec560de9f0889d45634dd03c5104a85
Gauri <68182889+GauriBhasker@users.noreply.github.com>
2023-10-17 13:48:28 +0000

    operations

---
+++ js_basics/operations.js
@@ -0,0 +1,9 @@
+console.log(1+2);     //3
+console.log("1"+2+3);   //123
+console.log("1"+2+"3");   //123
+console.log("1"+"2"+3);   //123
+console.log(1+2+"3");    //33
+
+console.log("Arun"  + " Paayal")
+
+console.log(1+2+" cm")
