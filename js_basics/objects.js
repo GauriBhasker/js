3040000f6409ea9027a5ee590b804bb3b31a6c11
Gauri <68182889+GauriBhasker@users.noreply.github.com>
2023-10-18 10:37:53 +0000

    objects

--- js_basics/objects.js
+++ js_basics/objects.js
@@ -48,3 +48,59 @@ const newuser = {
     name: "Arunita",
     age: 34,
 }
+
+console.log(newuser);
+console.log(newuser.hasOwnProperty('name'))
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+
+const obj2 = {
+    3:"c",
+    4:"d"
+}
+
+const obj3 = {
+    5:"e",
+    6:"f"
+}
+
+// newobj = Object.assign({}, obj1, obj2, obj3)
+// newobj = {obj1, obj2}
+
+newobj = {...obj1, ...obj2, ...obj3}
+
+console.log(newobj);
+
+console.log(Object.keys(newobj));
+console.log(Object.values(newobj));
+
+
+const usernames = [
+    {
+        username1: "Anil",
+        age1: 19
+    },
+    {
+        username2: "Arun",
+        age2: 22
+    },
+    {
+        username3: "Aditya",
+        age3: 67
+    },
+    {
+        username4: "Aashish",
+        age4: 34
+    }
+]
+
+console.log(usernames[0])
+console.log(usernames[1])
+console.log(usernames[2])
+console.log(usernames[3])
+
+console.log(Object.keys(usernames));
+console.log(Object.entries(usernames));
