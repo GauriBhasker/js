bf2ef99a32f40ad3cfe3a9ef697cb7e2b62ea6ea
Gauri <68182889+GauriBhasker@users.noreply.github.com>
2023-10-19 08:14:35 +0000

    functions

---
+++ js_basics/functions.js
@@ -0,0 +1,22 @@
+function printhello()
+{
+    return "hello"
+}
+
+let hello = printhello()
+
+console.log(hello)
+
+function sayhi()
+{
+    console.log("hi");
+}
+
+sayhi()
+
+function add(num1, num2){
+    console.log(num1 + num2)
+}
+
+add(6,7)
+
